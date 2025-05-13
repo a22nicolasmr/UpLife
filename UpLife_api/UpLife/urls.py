f4715a616469532e3f2899ac9a965d857c860796
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-05-13 14:20:57 +0200

    acabadas funcionalidades principales

--- UpLife_api/UpLife/urls.py
+++ UpLife_api/UpLife/urls.py
@@ -18,9 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import JsonResponse
+def benvida(request):
+    return JsonResponse({"mensaxe": "UpLife API funcionando correctamente 🚀"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('UpLife_api.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', benvida),  # ← Aquí engades a vista
+    path('', include('UpLife_api.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
