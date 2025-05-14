88af2258c4bb7f0b7e92c15551527877b1a6e19e
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-05-14 11:05:03 +0200

    cambiada ruta

--- UpLife_api/UpLife/settings.py
+++ UpLife_api/UpLife/settings.py
@@ -116,6 +116,8 @@ DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
+print("DATABASE_URL actual:", os.environ.get("DATABASE_URL"))
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
