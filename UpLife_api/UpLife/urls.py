26a45a0a29b90c79e2701adad5681f58512079e5
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-05-14 08:15:27 +0200

    ruta imaxes cambiada

--- UpLife_api/UpLife/urls.py
+++ UpLife_api/UpLife/urls.py
@@ -19,12 +19,25 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.http import JsonResponse
+from django.urls import re_path
+from django.views.static import serve
+
 def benvida(request):
     return JsonResponse({"mensaxe": "UpLife API funcionando correctamente 🚀"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', benvida),  # ← Aquí engades a vista
+    # path('', benvida),  # ← Aquí engades a vista
     path('', include('UpLife_api.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
+    
